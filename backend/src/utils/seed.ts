d5f8ecc1212dfc76de4a3b1d51e5625327905fa5
wesleyDM14 <wesleyfga@hotmail.com>
2024-11-23 15:07:07 -0300

    Update Populate DB script

--- backend/src/utils/seed.ts
+++ backend/src/utils/seed.ts
@@ -1,6 +1,7 @@
 import prismaClient from "./prismaClientProvider";
 
 async function seed() {
+
     const drivers = [
         { id: 1, nome: 'Homer Simpson', descricao: 'Olá! Sou o Homer, seu motorista camarada! Relaxe e aproveite o passeio, com direito a rosquinhas e boas risadas (e talvez alguns desvios).', carro: 'Plymouth Valiant 1973 rosa e enferrujado', avaliacao: '2/5\nMotorista simpático, mas errou o caminho 3 vezes. O carro cheira a donuts.', taxa: 2.5, km_min: 1 },
         { id: 2, nome: 'Dominic Toretto', descricao: 'Ei, aqui é o Dom. Pode entrar, vou te levar com segurança e rapidez ao seu destino. Só não mexa no rádio, a playlist é sagrada.', carro: 'Dodge Charger R/T 1970 modificado', avaliacao: '4/5\nQue Viagem incrível! O carro é um show a parte e o motorista, apesar de ter cara de poucos amigos, foi super gente boa. Recomendo!', taxa: 5, km_min: 5 },
@@ -8,18 +9,27 @@ async function seed() {
     ];
 
     for (const driver of drivers) {
-        await prismaClient.driver.upsert({
+        const createdDriver = await prismaClient.driver.upsert({
             where: { id: driver.id },
             update: {},
             create: driver,
         });
+
+        await prismaClient.avaliation.create({
+            data: {
+                rating: parseInt(driver.avaliacao.split('/')[0].trim()),
+                comment: driver.avaliacao.split('\n')[1],
+                driverId: createdDriver.id,
+            },
+        });
     }
+
     console.log('Banco de dados populado com sucesso!');
 }
 
 seed()
-    .catch((e) => {
-        console.error('Erro ao popular o banco de dados: ', e);
+    .catch((error) => {
+        console.error('Erro ao popular o banco de dados: ', error);
         process.exit(1);
     })
     .finally(async () => {
